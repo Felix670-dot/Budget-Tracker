ebd1e5e9a34ef65ed6d39f55a9a44833b0e70d02
felix <felixperez233@gmail.com>
2025-11-25 17:23:10 -0500

    Made some changes for vercel cloud server

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
-app.listen(PORT, () => {
+export default app;
+/*app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+});*/
