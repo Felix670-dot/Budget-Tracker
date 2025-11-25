de27ebe5a5792bdbeee6b560d6800b0b75048e32
felix <felixperez233@gmail.com>
2025-11-25 17:45:41 -0500

    changes that will handle if the connection server or local

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import transactionRoutes from './routes/transactions.js';
 
+/*
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+*/
 // Middleware
 app.use(cors());
 app.use(express.json());
