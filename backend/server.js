97db481219e6b5afb311b79793d629a08e73b590
felix <felixperez233@gmail.com>
2025-12-03 18:31:47 -0500

    Attempt three to be able to run on vercel

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,4 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 export default app; 
